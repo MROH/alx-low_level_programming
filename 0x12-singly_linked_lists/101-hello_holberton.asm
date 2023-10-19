3cb1adcd888a60725105a2e83af7516282164d7f
MROH <onyangohenry9@gmail.com>
2023-10-19 10:25:31 -0700

    Updated 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
        global    main
-          extern    printf 
+          extern    printf
 main:
-	  mov   edi, format	// Loading the address of the format string
-	  xor   eax, eax	// Clearing EAX register
-	  call  printf		// Calling the printf function
-	  mov 	eax, 0		//Set the return value to 0 
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
 	  ret
 format: db `Hello, Holberton\n`,0
